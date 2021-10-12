21789451724d57f54284b333b4bc1487d98f3d02
ruslan791 <danger791@bk.ru>
2021-10-12 17:46:29 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+dateOfBirthday = 1995  # Camal case
+date_of_birthday = 1993  # snake case
+var_int = 1  # Variable intejer (число)
+var_str = "Bakai"  # Variable string (строка)
+var_boolean_true = True  # Variable boolean (bool истина)
+var_boolean_false = False  # Variable boolean лож)
+var_list = [1, 3, 4, "Bakai", [1, 4, 5], (1, 4, 5), ]
+var_tuple = (1, [1, 2, ], "Bakai")
+var_dict = {"name": "Bakai", "date_of_birthday": "02.02.1999", "course": 3,
+            "physical_data": [90, 180], "python": True}  # Variable dictionary (cловарь)
+
+
+def print_hi():
+    print(var_tuple)
+    var_str = "Bakai"  # Variable string (строка)
+    var_boolean_true = True  # Variable boolean (bool истина)
+    var_boolean_false = False  # Variable boolean лож)
+    var_list = [1, 3, 4, "Bakai", [1, 4, 5], (1, 4, 5), ]
+    var_tuple = (1, [1, 2, ], "Bakai")
+    var_dict = {"name": "Bakai", "date_of_birthday": "22.12.1995", "course": 3,
+            "physycal_data": [85, 180], "python": True}  # Variable dictionary(cловарь)
+
+if __name__ == '__main__':
+    print_hi()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
